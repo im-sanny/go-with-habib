ff9ba68d411b7ffdab00781d96b2648bf8440409
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-07-17 22:15:37 +0600

    add: if else added in the basics

--- basics/main.go
+++ basics/main.go
@@ -2,13 +2,20 @@ package main
 
 import "fmt"
 
+func legalAge() {
+	age := 20
+
+	if age > 18 {
+		fmt.Println("Your're eligible to marriage!")
+	} else if age < 18 {
+		fmt.Println("You're not eligible for marriage!")
+	} else {
+		fmt.Println("Grow up kiddo!")
+	}
+}
+
 func main() {
-	/*
-	   int,
-	   string,
-	   bool,
-	   float
-	*/
+	/* int, string, bool, float */
 	a := 1
 	a = 2
 
@@ -19,4 +26,5 @@ func main() {
 	c = "Hello world!"
 
 	fmt.Println(a, b, c)
+	legalAge()
 }
