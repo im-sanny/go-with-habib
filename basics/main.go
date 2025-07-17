0ee85041ad17cf5c8341da06b82e10570505598a
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-07-17 22:54:30 +0600

    add: switch added in the basics

--- basics/main.go
+++ basics/main.go
@@ -5,12 +5,21 @@ import "fmt"
 func legalAge() {
 	age := 20
 
-	if age > 18 {
+	if age >= 18 {
 		fmt.Println("Your're eligible to marriage!")
 	} else if age < 18 {
 		fmt.Println("You're not eligible for marriage!")
 	} else {
 		fmt.Println("Grow up kiddo!")
+	}
+
+	switch {
+	case age == 15 || age == 16:
+		fmt.Println("Invalid")
+	case age == 20 || age >= 18:
+		fmt.Println("valid")
+	default:
+		fmt.Println("Bruh!")
 	}
 }
 
