7d9126ad8f2e801f70bb5563815eb3f9ef4251a0
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-16 13:46:07 +0600

    add: anonymous func & IIFE example added

--- funcs/main.go
+++ funcs/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 var a = 10
 
+// standard or named function
 func add(a, b int) {
 	fmt.Println(a + b)
 }
@@ -16,8 +17,15 @@ func init() {
 	// fmt.Println("I'm a func that executes first!")
 	fmt.Println(a)
 	a = 20
+
+	// anonymous func & IIFE
+	func(a int, b int) {
+		sum := a + b
+		fmt.Println(sum)
+	}(1, 2)
 }
 
 // standard func: function with name are standard or named function
 // init func: u can't call init func, program will call it by it's own, init function will be executed before any other function
-//
+// anonymous func: functions without name is anonymous func.
+// IIFE: if a function call/invoked immediately after writing then it's IIFE.
