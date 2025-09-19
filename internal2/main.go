20e5d3cff1d245708a2809c7a1e3d482aecb73f4
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-19 12:17:57 +0600

    add: added more notes and some codes in internal memory class 2

--- internal2/main.go
+++ internal2/main.go
@@ -3,7 +3,7 @@ package main
 
 import "fmt"
 
-const b = 10
+const a = 10
 
 var p = 100
 
@@ -13,21 +13,32 @@ func call() {
 		fmt.Println(z)
 	}
 	add(1, 1)
-	add(p, b)
+	add(p, a)
 }
 
 func main() {
 	call()
+	fmt.Println(a)
+}
+
+func init() {
+	fmt.Println("I'm init!")
 }
 
 /*
-	2 phase
-		1. compilation
-		2. execution
+	A go program runs in 2 phase:
+		1. compilation: here the code turns into binary executables, no code runs here.
+		2. execution: compiled binary files start to run here by maintaining execution order.
+
+
 
 		************** Compile phase ***************
 
 		** Code segment **
+		Code segment: compiled function and constant stay stored here as a part of executable instruction, code segment is read only.
+		Data segment: all the global variable stays here.
+		Function expression: at runtime this counts as object and stored in code segment.
+
 		a = 10
 		call= func(){...}
 		add = func(){...}
@@ -38,4 +49,30 @@ func main() {
 		go build main.go => compile it => main
 
 		./main
+*/
+
+/*
+	Visualization of compilation phase ->
+	go build main.go
+
+	compiler: checks syntax, scope and dependency.
+	saves:
+		> constant: a = 10
+		> global variable: p = 100
+		> function: init, main, call and anonymous function inside(add)
+		> generates necessary machine code and binary of metadata.
+
+	What's in the binary file:
+		> code segment: a, main, call and anonymous function.
+		> data segment: p
+		> no execution happens here.
+
+	Visualization of execution phase ->
+		1. init()            => "Hello"
+   	2. main()            => call() #calls the func
+   	3. call()            => add() #declare and call the func
+    		- add(5, 6)      => 11
+    		- add(100, 10)   => 110
+   	4. fmt.Println(a)    => 10
+
 */
