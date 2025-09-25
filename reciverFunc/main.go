d09062bb06f598985319b97b3da64568b0d2cdb7
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-25 11:43:07 +0600

    add: more func and notes added

--- reciverFunc/main.go
+++ reciverFunc/main.go
@@ -3,31 +3,35 @@ package main
 import "fmt"
 
 type User struct { // struct definition
-	Name string // member variable or property
+	Name string // property
 	Age  int
 }
 
-func printUserDetails(usr User) {
-	fmt.Println("Name:", usr.Name)
-	fmt.Println("Age:", usr.Age)
-}
-
-// receiver function
+// value receiver method (doesn't change original User)
 func (usr User) printDetails() {
 	fmt.Println("Name:", usr.Name)
 	fmt.Println("Age:", usr.Age)
 }
 
+// another method with a parameter
+func (usr User) call(a int) {
+	fmt.Println(usr.Name)
+	fmt.Println(a)
+}
+
 func main() {
-	var user1 User
-	user1 = User{
+	user1 := User{
 		Name: "Tom",
 		Age:  69,
 	}
 
-	printUserDetails(user1)
+	user1.printDetails()
+	user1.call(1)
 }
 
 /*
-Receiver func: receiver function can be used when u have custom type or struct
+	- Receiver func: Receiver functions can be used when you have a custom type or struct. They only work with custom types, and use of receiver functions is suggested with structs.
+	- Value receiver: gets a copy of the struct → changes inside don’t affect the original.
+	- Pointer receiver: gets the actual struct’s address → can modify the original data.
+	- Use receiver funcs when you want to give "behavior" to a struct.
 */
