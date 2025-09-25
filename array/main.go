3a171623e7fad071bbdfb184cf0eee13302cbed6
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-25 12:32:39 +0600

    add: added notes for making things more clear about array

--- array/main.go
+++ array/main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 var arr2 = [3]string{"Tom", "Jerry", "Butch"}
 
-
 func main() {
 	// var arr [2]int
 	// arr[0] = 5
@@ -15,3 +14,15 @@ func main() {
 	fmt.Println(arr)
 	fmt.Println(arr2)
 }
+
+/*
+Array:
+- An array is a data structure used to store a fixed-size sequence of elements of the same type.
+- In Go, array size is part of its type (e.g., [3]int and [4]int are different types).
+- Arrays are value types, meaning assignment or passing to a function creates a copy.
+- Default value: arrays are initialized with the zero value of their type (0 for int, "" for string, false for bool, etc.).
+- Indexing: array indices start at 0.
+- Length: use len(arr) to get array size.
+- Iteration: arrays can be iterated using for or for range.
+- Multidimensional: arrays can contain other arrays, e.g., [2][2]int.
+*/
