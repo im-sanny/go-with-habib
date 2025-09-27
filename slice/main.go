def328e27054e67833df434446580f4db93f1982
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-27 22:53:32 +0600

    add: put codes in new func to better readability

--- slice/main.go
+++ slice/main.go
@@ -2,13 +2,27 @@ package main
 
 import "fmt"
 
-func main(){
-	arr := [6]string{"This", "is", "a", "Go", "interview", "question"}
+func slice(){
+		arr := [6]string{"This", "is", "a", "Go", "interview", "question"}
 
-	s := arr[1:4]
+	s := arr[1:4]	//[is a go]
 	fmt.Println(s)
+
+	s1 := s[1:2] //[a] capacity is 4 bc it's using main array reference and if we count from a then we'll get the capacity.
+	fmt.Println(s1)
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
 }
 
+func main(){
+	slice()
+}
+
+
+/*
+	1. slice from an array
+	2. slice from a slice
+*/
 
 /*
 	In go a slice has 3 part:
