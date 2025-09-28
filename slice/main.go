68187e3a365774a7b17bedabb4104b65ad61814c
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-28 20:19:09 +0600

    add: added func that changes slice that passed into it

--- slice/main.go
+++ slice/main.go
@@ -14,6 +14,12 @@ import "fmt"
 // 	fmt.Println(cap(s1))
 // }
 
+func changeSlice(a []int) []int {
+	a[0] = 10
+	a = append(a, 11)
+	return a
+}
+
 func main() {
 	// s := []int{1, 2, 3} // if i remove fixed size from an array then it becomes slice, and now this calls slice literal
 	// fmt.Println("slice: ", s, "length: ", len(s), "capacity: ", cap(s))
@@ -37,17 +43,28 @@ func main() {
 	// s = append(s, 1, 2, 3) //[, 2, 3], len = 3, cap = 3
 	// fmt.Println(s)
 
-	var x []int      //[], len = 0, cap= 0
-	x = append(x, 1) //[1], len = 1, cap = 1
-	x = append(x, 2) //[1, 2] len = 2, cap = 2
-	x = append(x, 3) //[1, 2, 3] len = 3, cap = 4 // cap become 4 bc when theres no space left and there sth needs to add then it follows slice underlying rules and by that the cap expands.
-	fmt.Println(x)   //[1, 2, 3]
+	// var x []int      //[], len = 0, cap= 0
+	// x = append(x, 1) //[1], len = 1, cap = 1
+	// x = append(x, 2) //[1, 2] len = 2, cap = 2
+	// x = append(x, 3) //[1, 2, 3] len = 3, cap = 4 // cap become 4 bc when theres no space left and there sth needs to add then it follows slice underlying rules and by that the cap expands.
+	// fmt.Println(x)   //[1, 2, 3]
 
-	y := x
+	// y := x
 
-	x = append(x, 4) //[]
-	y = append(y, 5)
-	x[0] = 10
+	// x = append(x, 4) //[]
+	// y = append(y, 5)
+	// x[0] = 10
+
+	// fmt.Println(x)
+	// fmt.Println(y)
+
+	x := []int{1, 2, 3, 4, 5}
+	x = append(x, 6)
+	x = append(x, 7)
+
+	a := x[4:]
+
+	y := changeSlice(a)
 
 	fmt.Println(x)
 	fmt.Println(y)
