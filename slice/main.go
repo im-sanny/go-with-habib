2ffeb43f4951861c4a6005bd98242d6efc03c4c3
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-28 16:10:04 +0600

    add: added example and notes of slice with array underlying rules

--- slice/main.go
+++ slice/main.go
@@ -33,9 +33,24 @@ func main() {
 	// fmt.Println(len(s))
 	// fmt.Println(cap(s))
 
-	var s []int            //empty slice or nil slice //[], len = 0, cap = 0
-	s = append(s, 1, 2, 3) //[, 2, 3], len = 3, cap = 3
-	fmt.Println(s)
+	// var s []int            //empty slice or nil slice //[], len = 0, cap = 0
+	// s = append(s, 1, 2, 3) //[, 2, 3], len = 3, cap = 3
+	// fmt.Println(s)
+
+	var x []int      //[], len = 0, cap= 0
+	x = append(x, 1) //[1], len = 1, cap = 1
+	x = append(x, 2) //[1, 2] len = 2, cap = 2
+	x = append(x, 3) //[1, 2, 3] len = 3, cap = 4 // cap become 4 bc when theres no space left and there sth needs to add then it follows slice underlying rules and by that the cap expands.
+	fmt.Println(x)   //[1, 2, 3]
+
+	y := x
+
+	x = append(x, 4) //[]
+	y = append(y, 5)
+	x[0] = 10
+
+	fmt.Println(x)
+	fmt.Println(y)
 
 }
 
@@ -48,6 +63,7 @@ Kind of Slices:
 	5. make func with len and capacity
 	6. empty slice or nil slice
 	7. add value with append func
+	8. slice underlying array rules => 1024 -> 100% increase, until 1024 it will increase by 100%, after 1024 it will increase by 25%.
 */
 
 /*
@@ -59,4 +75,10 @@ Kind of Slices:
 		# max index of a 10 size array is 9 bc for array count starts with 0.
 		# in a 10 index array u can't define value s[10] = 55, if we try to do this then we'll get runtime error(index out of range).
 		# with append func we can add value in empty or in slice that already have value in it.
+*/
+
+/*
+	x = append(x, 1)
+	x = append(x, 2)
+	here append will expand the length and capacity of the array each time we append new value in it. and this is how we can expand length and capacity of the array without any issue.
 */
