0a92efd3037abe012949a90a5e983e86974e5171
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-28 11:46:38 +0600

    add: added slice literal and slice with make func

--- slice/main.go
+++ slice/main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func slice(){
-		arr := [6]string{"This", "is", "a", "Go", "interview", "question"}
+func slice() {
+	arr := [6]string{"This", "is", "a", "Go", "interview", "question"}
 
-	s := arr[1:4]	//[is a go]
+	s := arr[1:4] //[is a go]
 	fmt.Println(s)
 
 	s1 := s[1:2] //[a] capacity is 4 bc it's using main array reference and if we count from a then we'll get the capacity.
@@ -14,10 +14,18 @@ func slice(){
 	fmt.Println(cap(s1))
 }
 
-func main(){
+func main() {
+	// s := []int{1, 2, 3} // if i remove fixed size from an array then it becomes slice, and now this calls slice literal
+	// fmt.Println("slice: ", s, "length: ", len(s), "capacity: ", cap(s))
+
+	s := make([]int, 3) //[0, 0, 0] len = 3, cap =3
+	s[0] = 69           //[5, 0, 0] len = 3, cap =3
+	fmt.Println(s)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+
 	slice()
 }
-
 
 /*
 	1. slice from an array
