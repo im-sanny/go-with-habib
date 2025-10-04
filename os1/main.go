76659855a00b9d903bf30caa0a27962cc0d66f3e
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-10-04 23:02:49 +0600

    add: added some notes on computer architecture

--- os1/main.go
+++ os1/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	fmt.Println("Computer architecture!")
 }
+
+/*
+computer architecture:
+motherboard: motherboard in computer connects all the stuff together.
+RAM is a volatile/primary memory it forgets everything after reset or turn off the system.
+HDD/SSD: permanent/ memory, who doesn't forget anything until we remove from it manually.
+cpu: cpu does everything by doing only +, -, x, /, &, ||, !
+
+
+human histories first known computing tool is abacus.
+Gottfried Wilhelm Leibniz formalized the binary number system in 1703, laying key groundwork for its use in modern computing.
+Charles Babbage designed the Analytical Engine in the 1830s, considered the first conceptual general-purpose computer.
+*/
