5998d78179a76121deca67f932399b91bc46e258
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-11-13 17:10:20 +0600

    add: added advance routing to handle apis in better way

--- ecommerce/main.go
+++ ecommerce/main.go
@@ -6,14 +6,6 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "I'm a software engineer")
-}
-
 type Product struct {
 	ID          int
 	Title       string
@@ -25,26 +17,10 @@ type Product struct {
 var productList []Product
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
-	handleCors(w)
-	handlePreflightReq(w, r)
-
-	if r.Method != http.MethodGet { //r.Method = post, put, patch, delete
-		http.Error(w, "please give me GET request", 400)
-		return
-	}
-
 	sendData(w, productList, 200)
 }
 
 func createProduct(w http.ResponseWriter, r *http.Request) {
-	handleCors(w)
-	handlePreflightReq(w, r)
-
-	if r.Method != "POST" { //r.Method = post, put, patch, delete
-		http.Error(w, "please give me POST request", 400)
-		return
-	}
-
 	var newProduct Product
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newProduct)
@@ -55,22 +31,8 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 	}
 
 	newProduct.ID = len(productList) + 1
-
 	productList = append(productList, newProduct)
 	sendData(w, newProduct, 201)
-}
-
-func handleCors(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Method", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Name") // for this to work i need to set a custom header from frontend code
-	w.Header().Set("Content-Type", "application/json")
-}
-
-func handlePreflightReq(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodOptions {
-		w.WriteHeader(200)
-	}
 }
 
 func sendData(w http.ResponseWriter, data interface{}, statusCode int) {
@@ -82,14 +44,14 @@ func sendData(w http.ResponseWriter, data interface{}, statusCode int) {
 func main() {
 	mux := http.NewServeMux() //router
 
-	mux.HandleFunc("/hello", helloHandler) //route
-	mux.HandleFunc("/about", aboutHandler)
-	mux.HandleFunc("/products", getProducts)
-	mux.HandleFunc("/create-product", createProduct)
+	mux.Handle("GET /products", http.HandlerFunc(getProducts))
+	mux.Handle("POST /create-product", http.HandlerFunc(createProduct))
 
 	fmt.Println("Server running on :3000")
 
-	err := http.ListenAndServe(":3000", mux)
+	globalRouter := globalRouter(mux)
+
+	err := http.ListenAndServe(":3000", globalRouter)
 	if err != nil {
 		fmt.Println("Error starting the server", err)
 	}
@@ -99,41 +61,44 @@ func main() {
 func init() {
 	prod1 := Product{
 		ID:          1,
-		Title:       "Fruit 1",
-		Description: "Fruits are delicious",
-		Price:       5,
+		Title:       "Fresh Apples",
+		Description: "Crisp and sweet red delicious apples",
+		Price:       3.99,
 		ImgUrl:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRST6XqAF8KkLF0Xqj3vcyfESa4KCCj-Jswhg&s",
 	}
 	prod2 := Product{
 		ID:          2,
-		Title:       "Fruit 1",
-		Description: "Fruits are delicious",
-		Price:       5,
+		Title:       "Ripe Bananas",
+		Description: "Fresh yellow bananas, perfect for smoothies",
+		Price:       1.99,
 		ImgUrl:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRST6XqAF8KkLF0Xqj3vcyfESa4KCCj-Jswhg&s",
 	}
 	prod3 := Product{
 		ID:          3,
-		Title:       "Fruit 1",
-		Description: "Fruits are delicious",
-		Price:       5,
-		ImgUrl:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRST6XqAF8KkLF0Xqj3vcyfESa4KCCj-Jswhg&s",
-	}
-	prod4 := Product{
-		ID:          4,
-		Title:       "Fruit 1",
-		Description: "Fruits are delicious",
-		Price:       5,
-		ImgUrl:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRST6XqAF8KkLF0Xqj3vcyfESa4KCCj-Jswhg&s",
-	}
-	prod5 := Product{
-		ID:          5,
-		Title:       "Fruit 1",
-		Description: "Fruits are delicious",
-		Price:       5,
+		Title:       "Sweet Oranges",
+		Description: "Juicy oranges packed with vitamin C",
+		Price:       4.50,
 		ImgUrl:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRST6XqAF8KkLF0Xqj3vcyfESa4KCCj-Jswhg&s",
 	}
 
-	productList = append(productList, prod1, prod2, prod3, prod4, prod5)
+	productList = append(productList, prod1, prod2, prod3)
+}
+
+func globalRouter(mux *http.ServeMux) http.Handler {
+	handleAllReq := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Method", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Name") // for this to work i need to set a custom header from frontend code
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(200)
+			return
+		}
+		mux.ServeHTTP(w, r)
+
+	}
+	return http.HandlerFunc(handleAllReq)
 }
 
 /*
