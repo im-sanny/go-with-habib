29278ad5ae20e6cf119bb44682013b9651d136d6
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-11-24 21:41:50 +0600

    add&connect: added pgsql database setting and connected the api with database successfully

---
+++ ecommerce/infra/db/connection.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func GetConnectionString() string {
+	//user -> postgres
+	//password -> 360420
+	//host -> localhost
+	//port -> 5432
+	//db name -> ecommerce
+	return "user=postgres password=360420 host=localhost port=5432 dbname=ecommerce sslmode=disable"
+
+}
+
+func NewConnection() (*sqlx.DB, error) {
+	dbSource := GetConnectionString()
+	dbCon, err := sqlx.Connect("postgres", dbSource)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return dbCon, nil
+}
